98772d056caf71a6db8723ca9d0a7ac92be30bfa
ilzeelina <165931638+ilzeelina@users.noreply.github.com>
2024-04-04 20:24:28 +0300

    Update main.java

--- main.java
+++ main.java
@@ -25,3 +25,32 @@ public class Main {
 //  +5 to 15 you wear warm
 // 30 + cooling 
 }
+
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+
+    str
+    double temp = -10.23;
+
+    if (temp <= 5) {
+      System.out.println("Wear super warm");
+    } else if (temp <= 15) {
+      System.out.println("Wear warm");
+    } else if (temp <= 30) {
+      System.out.println("Wear normal");
+    } else {
+      System.out.println("You need cooling");
+    }
+
+  }
+
+  // @Test
+  // void addition() {
+  // assertEquals(2, 1 + 1);
+  // }
+}
